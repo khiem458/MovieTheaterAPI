9c8bd904bd242098624bd24bd11a3b7eff3b3efd
khiem458 <lightningboy458@gmail.com>
2024-07-01 14:04:49 +0700

    New

---
+++ src/main/java/com/fpt/MovieTheaterAPI/MovieTheaterApiApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.MovieTheaterAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheaterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheaterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/MovieTheaterAPI/MovieTheaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.MovieTheaterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
